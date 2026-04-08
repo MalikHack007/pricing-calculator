c417e646c117d28a05a6eb302b4a4ab5f4394884
mzhang <mzhang@Regions-Mac-Pro.local>
2026-04-08 16:07:08 -0500

    Finished implementation of pricing logic

--- app/date-input/page.tsx
+++ app/date-input/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { ReactElement, useState } from "react";
 
 export default function DateForm(){
     //needed states
@@ -8,19 +8,39 @@ export default function DateForm(){
     const [endDate, setEndDate] = useState<string>("");
 
     function calculatePricing(start_date:Date, end_date:Date){
+        //convert the string states into actual date objects
+        const nightly_rate = 55
+        const start = new Date(start_date);
+        const end = new Date(end_date);
+        const diff = (end.getTime() - start.getTime()) / 1000;
+        const day_seconds = 60*60*24;
+        //calculate the units
+        //less than 24 hr stay
+        if (diff < day_seconds){
+            return nightly_rate
+        }
         
+        const nights = Math.floor(diff/day_seconds);
+        const extended_care_units = diff % day_seconds;
+        if (extended_care_units < 2*60*60){
+            return nights * nightly_rate
+        }
+        else if(extended_care_units >= 2*60*60 && extended_care_units <= 8*60*60){
+            return nights * nightly_rate + 0.5*nightly_rate
+        }
+        else{
+            return (nights + 1) * nightly_rate
+        }
     }
 
-    function handleSubmit(){
-        //convert the string states into actual date objects
-        const start = new Date(startDate);
-        const end = new Date(endDate);
-
-        
+    function handleSubmit(e:React.SubmitEvent){
+        e.preventDefault();
+        const total_price = calculatePricing(new Date(startDate), new Date(endDate));
+        console.log(`Total Price: ${total_price}`);
     }
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Start</label>
             <input type="datetime-local" name="start" value={startDate} onChange={(e)=> {
                 console.log(typeof e.target.value)
